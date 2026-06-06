769c30d8c9d41e52fb6b489decd7d48cb4de5e4f
KetanMulani <ketanmulani27@gmail.com>
2026-06-06 09:53:54 +0530

    update landing page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,65 +1,63 @@
-import "../styles/footer.css";
-
 function Footer() {
 
     return(
-        <footer className="footer">
-            <div className="footer-top">
-                <div className="footer-left">
-                    <h2>Cyber.AI - Cyber Punk Website</h2>
-                    <p>
+        <footer className="bg-black text-white font-sans px-5 sm:px-10 lg:px-20 py-16 w-full">
+            <div className="flex flex-col md:flex-row  gap-30 justify-evenly pb-16">
+                <div className="max-w-[320px]">
+                    <h2 className="text-[22px] font-bold mb-5">Cyber.AI - Cyber Punk Website</h2>
+                    <p className="text-[14px] text-gray-300 leading-7">
                         Explore the high-tech,low life world where the lines between humanity
                     </p>
                 </div>
 
 
 
-                <div class="footer-links">
-                    <h3>Information</h3>
-                    <a href="#">Press center</a>
-                    <a href="#">Our Blog</a>
-                    <a href="#">Terms and Condition</a>
+                <div className="flex flex-col gap-3">
+                    <h3 className="text-[14px] font-semibold ">Information</h3>
+                    <a href="#" className="text-gray-300 hover:text-orange-500 transition">Press center</a>
+                    <a href="#" className="text-gray-300 hover:text-orange-500 transition">Our Blog</a>
+                    <a href="#" className="text-gray-300 hover:text-orange-500 transition min-w-40">Terms and Condition</a>
                 </div>
 
 
 
-                <div class="footer-links">
+                <div className="flex flex-col gap-3 ">
                     <h3>Menu</h3>
-                    <a href="#">About</a>
-                    <a href="#">Services</a>
-                    <a href="#">Blog</a>
+                    <a href="#" className="text-gray-300 hover:text-orange-500 transition">About</a>
+                    <a href="#" className="text-gray-300 hover:text-orange-500 transition">Services</a>
+                    <a href="#" className="text-gray-300 hover:text-orange-500 transition">Blog</a>
                 </div>
 
 
-                <div class="footer-links">
+                <div className="flex flex-col gap-3">
                     <h3>Contact</h3>
-                    <a href="#">Phone : +123 456 789</a>
-                    <a href="#">Email: @example.com</a>
-                    <a href="#">Address Line 01</a>
+                    <a href="#" className="text-gray-300 hover:text-orange-500 transition">Phone : +123 456 789</a>
+                    <a href="#" className="text-gray-300 hover:text-orange-500 transition">Email: @example.com</a>
+                    <a href="#" className="text-gray-300 hover:text-orange-500 transition">Address Line 01</a>
                 </div>
             </div>
 
 
-            <div className="footer-bottom">
-                <div className="footer-bottom-left">
-                    <p>&#169; Cyber.Game-Ketan Mulani 2026.All rights reserved</p>
+            <div className="flex flex-col lg:flex-row justify-between items-start lg:items-center gap-8 border-t border-gray-800 pt-8 ">
+                <div className="text-[14px]">
+                    <p className="md:pl-15">&#169; Cyber.Game-Ketan Mulani 2026.All rights reserved</p>
                 </div>
-                <div className="footer-policy">
-                    <a href="#" className="policy-1">Terms</a>
-                    <a href="#">Privacy</a>
-                    <a href="#">Cookies</a>
+                <div className="flex gap-6 flex-wrap">
+                    <a href="#" className="hover:text-orange-500">Terms</a>
+                    <a href="#" className="hover:text-orange-500">Privacy</a>
+                    <a href="#" className="hover:text-orange-500">Cookies</a>
                 </div>
                 
                 
-                <div className="footer-social">
-                    <div className="social-circle1">
-                        <i class="fa-brands fa-linkedin-in"></i>
+                <div className="flex gap-6">
+                    <div className="cursor-pointer hover:text-orange-500 ">
+                        <i class="fa-brands fa-linkedin-in "></i>
                     </div>
-                    <div className="social-circle2">
+                    <div className="cursor-pointer hover:text-orange-500 ">
                         <i class="fa-brands fa-instagram"></i>
                     </div>
 
-                    <div className="social-circle3">
+                    <div className="cursor-pointer hover:text-orange-500 ">
                         <i class="fa-brands fa-x-twitter"></i>
                     </div>
                 </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,29 +1,46 @@
-import "../styles/hero.css";
 import heroImage from "../assets/hero_image.png";
+import "../styles/hero.css";
 
+function Hero() {
+  return (
+    <section className="bg-black text-white flex flex-col lg:flex-row justify-start items-center gap-10 lg:gap-16 px-6 md:px-10 lg:pl-[100px] pt-10 md:pt-16 lg:pt-[80px]">
+      <div className="hero-image">
+        <img
+          src={heroImage}
+          alt="Cyberpunk"
+          className="w-full max-w-[320px] sm-max-w-[420px] md:max-w-[500px] lg:max-w-[620px] al:max-w-[700px] object-cover"
+        />
+      </div>
 
-function Hero () {
+      <div className="max-w-[600px] p-4 md:p-8 lg:p-[50px] text-center lg:text-left">
+        <h1 className="text-[30px] md:text-[45px] lg:text-[55px] leading-[1.3] font-bold mb-8 lg:mb-[50px] font-['Orbitron']">
+          Your{" "}
+          <span className="bg-gradient-to-r from-[#ff5e00] via-[#fda103] to-[#fb7303] bg-clip-text text-transparent font-['Orbitron']">
+            Portal to a Gritty,
+          </span>
+          {" "}Tech-Driven Future
+        </h1>
 
-    return(
-        <section className="hero">
-            <div className="hero-image">
-                <img src={heroImage} alt="Cyberpunk"/>
-            </div>
+        <p className="text-[15px] leading-8 text-gray-300 mb-5 font-sans">
+          A realm where advanced technology meets dystopian reality.
+          Our website is your gateway to a universe of neon lights,
+          gritty streets, and cybernetic enhancements.
+        </p>
 
-            <div className="hero-content">
-                <h1>Your <span>Portal to a Gritty,</span>Tech-Driven Future</h1>
-                <p>
-                    A realm where advanced technology meets dystopian reality. Our website is your gateway to a universe of neon lights,gritty streets, and cybernetic enhancements.
-                </p>
-                 <div className="hero-buttons">
-                <button className="btn hvr-sweep-to-right btn1">Read More</button>
-                <button className="btn btn2">Learn More</button>
-            </div>
-            </div>
+        <div className="flex flex-col sm:flex-row gap-5 mt-8 justify-center lg:justify-start">
 
-        </section>
-    );
+          <button className="px-5 py-2 rounded-lg text-white text-[18px] bg-gradient-to-r from-[#f8961e] via-[#ff6b00] to-[#f94144] hover:-translate-y-[2px] transition-all duration-300">
+            Read More
+          </button>
+
+          <button className=" px-5 py-2 btn2">
+            Learn More
+          </button>
+    
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Hero;
-
